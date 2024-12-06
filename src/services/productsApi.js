33e7604b22d9cc2ef3dd6dbfb9c2ca7ca46c6285
saad-rahid <saadalrahid13@gmail.com>
2024-12-06 13:15:46 +0600

    Add delete method

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -3,16 +3,26 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5174/" }),
+  tagTypes: ["Product"],
 
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => "products",
+
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Product", id })),
+              { type: "Product", id: "LIST" },
+            ]
+          : [{ type: "Product", id: "LIST" }],
     }),
     deleteProducts: builder.mutation({
       query: (id) => ({
         url: `products/${id}`,
         method: "DELETE",
       }),
+      invalidatesTags: (result, error, id) => [{ type: "Product", id }],
     }),
   }),
 });
